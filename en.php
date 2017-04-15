87c7eef4f0d800437a69b037949e98421c2193e4
moodalagiri <manasa.moodalagiri@gmail.com>
2017-04-15 16:28:44 -0700

    first commit

---
+++ en.php
@@ -0,0 +1,16 @@
+<?php
+define("VR","VR market Place");
+define("HM","Home");
+define("AU","About Us");
+define("SV","Services");
+define("DL","Download");
+define("DB","Dashboard");
+define("CU","Contact Us");
+define("TR","Translate");
+define("LO","Logout");
+define("DS","One stop destination to play and explore the world of Virtual Reality ");
+define("ZO","Enter the VR Zone!!...");
+define("QE","Have a question or need a custom quote?");
+define("SP","Do not worry! We are here to help you. Please get in touch with the support team at support@vrmarketplace.com");
+define("RI",". All Rights Reserved.");
+?>
---
+++ es.php
@@ -0,0 +1,16 @@
+<?php
+define("VR","Mercado VR");
+define("HM","Casa");
+define("AU","Sobre nosotros");
+define("SV","Servicios");
+define("DL","Descargar");
+define("DB","Tablero");
+define("CU","Contáctenos");
+define("TR","Traducir");
+define("LO","Cerrar sesión");
+define("DS","Un destino de parada para jugar y explorar el mundo de la Realidad Virtual");
+define("ZO","¡Entre en la Zona VR !! ....");
+define("QE","¿Tiene alguna pregunta o necesita un presupuesto personalizado?");
+define("SP","¡No te preocupes! Estamos aquí para ayudarte. Por favor, póngase en contacto con el equipo de soporte técnico en support@vrmarketplace.com");
+define("RI",". Todos los derechos reservados");
+?>
